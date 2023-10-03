bfd1acc7438b7c8520d4c0e010a58dc87cddb009
Vladislav Yarmak <vladislav-ex-src@vm-0.com>
2023-10-04 01:37:30 +0300

    initial code

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Snawoot/rlzone
+
+go 1.21.1
---
+++ rlzone.go
@@ -0,0 +1,85 @@
+package rlzone
+
+import (
+	"sync"
+	"time"
+)
+
+type RatelimitZone[K comparable] struct {
+	prevMap      map[K]uint32
+	currMap      map[K]uint32
+	prevWndStart time.Time
+	currWndStart time.Time
+	window       time.Duration
+	limit        uint32
+	mux          sync.Mutex
+}
+
+func New[K comparable](window time.Duration, limit uint32) *RatelimitZone[K] {
+	return &RatelimitZone[K]{
+		prevMap: make(map[K]uint32),
+		currMap: make(map[K]uint32),
+		window:  window,
+	}
+}
+
+func (z *RatelimitZone[K]) Allow(key K) bool {
+
+	now := time.Now().Truncate(0)
+	reqCurrWndStart := now.Truncate(z.window)
+	reqPrevWndStart := reqCurrWndStart.Add(-z.window)
+
+	z.mux.Lock()
+	defer z.mux.Unlock()
+
+	val := z.getWindowValue(key, reqPrevWndStart, reqCurrWndStart, now)
+	if float64(z.limit) <= val {
+		return false
+	}
+
+	z.orderMaps(reqPrevWndStart, reqCurrWndStart)
+
+	z.incCounter(key, reqCurrWndStart)
+
+	return true
+}
+
+func (z *RatelimitZone[K]) orderMaps(prevStart, currStart time.Time) {
+	newCurrMap := z.getWndMap(currStart, true)
+	newPrevMap := z.getWndMap(prevStart, true)
+	z.currMap = newCurrMap
+	z.prevMap = newPrevMap
+}
+
+func (z *RatelimitZone[K]) getWindowValue(key K, prevWndStart, currWndStart, now time.Time) float64 {
+	prevCtr := z.getCounter(key, prevWndStart)
+	currCtr := z.getCounter(key, currWndStart)
+	multiplier := 1 - (float64(now.Sub(currWndStart)) / float64(z.window))
+	return float64(prevCtr)*multiplier + float64(currCtr)
+}
+
+func (z *RatelimitZone[K]) getCounter(key K, wndStart time.Time) uint32 {
+	if m := z.getWndMap(wndStart, false); m != nil {
+		return m[key]
+	}
+	return 0
+}
+
+func (z *RatelimitZone[K]) incCounter(key K, wndStart time.Time) {
+	if m := z.getWndMap(wndStart, false); m != nil {
+		m[key]++
+	}
+}
+
+func (z *RatelimitZone[K]) getWndMap(wndStart time.Time, create bool) map[K]uint32 {
+	switch {
+	case wndStart == z.currWndStart:
+		return z.currMap
+	case wndStart == z.prevWndStart:
+		return z.prevMap
+	}
+	if create {
+		return make(map[K]uint32)
+	}
+	return nil
+}
